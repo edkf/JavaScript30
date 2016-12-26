a7eb74950d7aee84959accdd70d2235a9e8aa7b7
Edgard Kozlowski <edgardfernandes11@gmail.com>
2016-12-25 23:15:31 -0200

    11

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -1,11 +1,15 @@
 // Selectors
-const player = document.querySelector('.player__video');
-const video = document.querySelector('.player__video');
-const playButton = document.querySelector('.player__button');
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-function playPauseVideo(){
+// Functions
+
+function playPauseVideo() {
   if (video.paused) {
     video.play();
   } else {
@@ -13,11 +17,45 @@ function playPauseVideo(){
   }
 }
 
-function changeVolume() {
-
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
 }
 
-// Play/Pause vídeo clicking in the button and the whole player
-player.addEventListener('click', playPauseVideo);
-playButton.addEventListener('click', playPauseVideo);
-// volume.addEventListener('input', changeVolume);
+function skipVideo() {
+  console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name + ' ' + this.value);
+}
+
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  // console.log(scrubTime);
+  video.currentTime = scrubTime;
+}
+
+// Events
+video.addEventListener('click', playPauseVideo);
+toggle.addEventListener('click', playPauseVideo);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skipVideo));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', function(){mousedown = true;});
+progress.addEventListener('mouseup', function(){mousedown = false;});
